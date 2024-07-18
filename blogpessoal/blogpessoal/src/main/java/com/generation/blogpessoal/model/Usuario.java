c81541d37f637ab15fb6f077292ff6605cd8a5ea
Gabriella Jacubavicius <gabriella_jacubavicius@hotmail.com>
2024-07-18 15:38:02 -0300

    test: implementação de testes com JUnit

--- blogpessoal/blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ blogpessoal/blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -38,11 +38,24 @@ public class Usuario {
 	
 	@Size(max = 5000, message = "O link da foto não pode ser maior do que 5000 caracteres")
 	private String foto;
-	
+
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
 
+	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+	
+	
+	public Usuario() {
+		
+	}
+	
 	public Long getId() {
 		return id;
 	}
